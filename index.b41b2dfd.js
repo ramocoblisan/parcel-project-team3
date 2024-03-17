1f3d6f370437f316c608d25ff802ba7f3d5ffef1
ramocoblisan <142051946+ramocoblisan@users.noreply.github.com>
2024-03-17 20:25:27 +0000

    Deploying to gh-pages from @ ramocoblisan/parcel-project-team3@5173de622d9ffec149db37bcbf4579016d399e14 🚀

---
+++ index.b41b2dfd.js
@@ -0,0 +1,2 @@
+function showElement(e){document.querySelectorAll(".element").forEach((function(e){e.style.display="none"})),document.getElementById(e).style.display="block"}
+//# sourceMappingURL=index.b41b2dfd.js.map
